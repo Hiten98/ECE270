6b9d3ef6ac5446ccd99e169cd12fcc3253aa5431
Hiten Rathod <hitenrathod98@gmail.com>
2018-04-23 18:41:45 -0400

    ring counter complete

--- ring_counter.v
+++ ring_counter.v
@@ -1,7 +1,21 @@
 // 4 bit ring counter
 module ring_counter(CLK, R, Q);
-  begin
+  // declarations
+  input wire CLK, R;
+  output reg [3:0] Q;
+  reg [3:0] next_Q;
+
   always @ (posedge CLK)
     begin
+      Q <= next_Q;
     end
-  end
+
+  always @ (Q)
+    begin
+      next_Q[3] = ~R & Q[2];
+      next_Q[2] = ~R & Q[1];
+      next_Q[1] = ~R & Q[0];
+      next_Q[0] = ~R & Q[3] | R;
+    end
+    
+endmodule
