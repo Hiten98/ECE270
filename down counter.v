efe598e45e4825a39c8feb3aa084472c0bea8d83
Hiten Rathod <hitenrathod98@gmail.com>
2018-04-24 01:00:51 -0400

    Up and down counters

---
+++ down counter.v
@@ -0,0 +1,20 @@
+module count8d(CLK, Q);
+  input wire CLK;
+  output reg [7:0] Q;
+  reg [7:0] next_Q;
+  always @ (posedge CLK)
+    begin
+      Q <= next_Q;
+    end
+  always @ (Q)
+    begin
+      next_Q[0] = ~Q[0];
+      next_Q[1] = Q[1] ^ ~Q[0];
+      next_Q[2] = Q[2] ^ (~Q[1] & ~Q[0]);
+      next_Q[3] = Q[3] ^ (~Q[2] & ~Q[1] & ~Q[0]);
+      next_Q[4] = Q[4] ^ (~Q[3] & ~Q[2] & ~Q[1] & ~Q[0]);
+      next_Q[5] = Q[5] ^ (~Q[4] & ~Q[3] & ~Q[2] & ~Q[1] & ~Q[0]);
+      next_Q[6] = Q[6] ^ (~Q[5] & ~Q[4] & ~Q[3] & ~Q[2] & ~Q[1] & ~Q[0]);
+      next_Q[7] = Q[7] ^ (~Q[6] & ~Q[5] & ~Q[4] & ~Q[3] & ~Q[2] & ~Q[1] & ~Q[0]);
+    end
+endmodule
